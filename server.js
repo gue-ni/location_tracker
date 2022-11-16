297df000eb2dce20fdce25a4a5a1c31e7da95578
gue-ni <jakob.g.maier@gmail.com>
2022-11-16 09:47:14 +0000

    refactor

--- server.js
+++ server.js
@@ -11,48 +11,50 @@ app.use(express.json());
 app.use(morgan('combined'));
 
 function get_latest(){
-    let latest = null;
-    try {
-        const file = fs.readFileSync(filename);
-        let locations = JSON.parse(file);
-        latest = locations[locations.length - 1]
-      } catch(e) {
-        console.log(e);
-      }
-    return latest;
+  let latest = null;
+  try {
+      const file = fs.readFileSync(filename);
+      const locations = JSON.parse(file);
+      latest = locations[locations.length - 1]
+  } catch(e) {
+      console.log(e);
+  }
+  return latest;
 }
 
 function write_location(data){
-    if (data){
-      let locations = [];
+  if (!data){
+    return;
+  }
 
-      try {
-        const file = fs.readFileSync(filename);
-        locations = JSON.parse(file);
-      } catch(e) {
-        console.log(e);
-      }
+  let locations = [];
 
-      locations.push(data);
+  try {
+    const file = fs.readFileSync(filename);
+    locations = JSON.parse(file);
+  } catch(e) {
+    console.log(e);
+  }
 
-      try {
-        fs.writeFileSync(filename, JSON.stringify(locations));
-      }  catch(e) {
-        console.log(e);
-      }
-    }
+  locations.push(data);
+
+  try {
+    fs.writeFileSync(filename, JSON.stringify(locations));
+  }  catch(e) {
+    console.log(e);
+  }
 }
 
 app.get('/latest', (req, res) => {
-    let latest = get_latest();
-    if (latest){
-         let zoom = 13;
+  let latest = get_latest();
+
+  if (latest){
+    let zoom = 13;
     let url = `https://www.google.com/maps/place/${latest.lat},${latest.lon}/@${latest.lat},${latest.lon},${zoom}z`;
     res.redirect(url);
-    } else {
-        res.status(404).send();
-    }
-   
+  } else {
+    res.status(404).send();
+  }
 });
 
 app.get('*', (req, res) => {
@@ -60,15 +62,15 @@ app.get('*', (req, res) => {
 })
 
 app.post('*', (req, res) => {
-    const data = req.body;
+  const data = req.body;
 
-    write_location(data);
+  write_location(data);
 
-    res.json([]);
+  res.json([]);
 })
 
 app.listen(port, () => {
-    console.log(`Location Tracker app listening on port ${port}`)
+  console.log(`Location Tracker app listening on port ${port}`)
 });
 
 
