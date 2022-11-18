7eef2cd26726d975e1a2a5529143e832f1ff4cb9
Jakob <45669207+gue-ni@users.noreply.github.com>
2022-11-18 10:31:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -23,23 +23,23 @@ db.serialize(() => {
 
 app.use(express.json());
 app.use(morgan('combined'));
-app.use(express.static('public/heatmap'))
+app.use(express.static('public/heatmap'));
 
 function all(sql, params) {
   return new Promise((resolve, reject) => {
     db.all(sql, params, (err, rows) => {
-      if (err) reject(err)
+      if (err) reject(err);
       resolve(rows);
-    })
+    });
   });
 }
 
 function get(sql, params) {
   return new Promise((resolve, reject) => {
     db.get(sql, (err, row) => {
-      if (err) reject(err)
+      if (err) reject(err);
       resolve(row);
-    })
+    });
   });
 }
 
@@ -74,29 +74,31 @@ function googleMaps(lat, lon, zoom) {
   return `https://www.google.com/maps/place/${lat},${lon}/@${lat},${lon},${zoom}z`;
 }
 
+// Redirect to last known location on google maps.
 app.get('/latest', async (req, res, next) => {
   try {
     const latest = await getLastCoordinates();
     if (latest) {
       return res.redirect(googleMaps(latest.lat, latest.lon, 15));
-      //return res.json(latest)
     } else {
       return res.status(400).send();
     }
   } catch (err) {
-    next(err)
+    next(err);
   }
 });
 
+// Returns location data. Timestamp removed and randomized for privacy. 
 app.get('/locations', async (req, res, next) => {
   try {
     const locations = await all("SELECT lat, lon FROM locations ORDER BY RANDOM()");
     return res.json(locations);
   } catch (err) {
-    next(err)
+    next(err);
   }
 })
 
+// Returns status.
 app.get('/status', async (req, res, next) => {
   try {
     const { count } = await get('SELECT COUNT(*) AS count FROM locations');
@@ -107,6 +109,7 @@ app.get('/status', async (req, res, next) => {
   }
 })
 
+// Save location data in database.
 app.post('*', async (req, res, next) => {
   try {
     const data = req.body;
@@ -115,12 +118,12 @@ app.post('*', async (req, res, next) => {
       const { lat, lon, tst, tid } = data;
       await writeLocation(lat, lon, tst, tid);
     }
-
+    // Owntracks expects an empty json array as reponse.
     return res.status(200).json([]);
   } catch (err) {
-    console.log(err)
+    // Owntracks sometimes repeats messages. Already saved timestamp/location tuples shall be ignored.
     if (err.code == "SQLITE_CONSTRAINT") {
-      console.error(err.code)
+      console.error(err.code);
       return res.status(200).json([]);
     } else {
       return next(err);
@@ -131,7 +134,7 @@ app.post('*', async (req, res, next) => {
 const errorHandler = (err, req, res, next) => {
   const errStatus = err.statusCode || 500;
   console.error(err)
-  return res.status(500).json([])
+  return res.status(errStatus).json([])
 }
 
 app.use(errorHandler);
