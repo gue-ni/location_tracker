b09130acf262ce25cc7854e09eb0e3e1551e3033
gue-ni <jakob.g.maier@gmail.com>
2022-11-18 08:44:44 +0000

    udpate

--- server.js
+++ server.js
@@ -24,13 +24,22 @@ db.serialize(() => {
 
 app.use(express.json());
 app.use(morgan('combined'));
-app.use(express.static('public'))
+app.use(express.static('public/heatmap'))
 
-function all(sql) {
+function all(sql, params) {
   return new Promise((resolve, reject) => {
-    db.all(sql, (err, rows) => {
+    db.all(sql, params, (err, rows) => {
       if (err) reject(err)
       resolve(rows);
+    })
+  });
+}
+
+function get(sql, params) {
+  return new Promise((resolve, reject) => {
+    db.get(sql, (err, row) => {
+      if (err) reject(err)
+      resolve(row);
     })
   });
 }
@@ -44,50 +53,91 @@ function run(sql, params) {
   });
 }
 
-function get_all() {
-  const sql = "SELECT lat, lon, DATETIME(tst, 'auto') AS dt, tid FROM locations";
-  return all(sql);
+function getCoordinates() {
+  return all("SELECT lat, lon, DATETIME(tst, 'auto') AS dt, tid FROM locations");
 }
 
-function write_location(lat, lon, tst, tid) {
+function writeLocation(lat, lon, tst, tid) {
   const sql = "INSERT INTO locations (lat, lon, tst, tid) VALUES (?, ?, ?, ?)";
   return run(sql, [lat, lon, tst, tid]);
 }
 
-function google_maps(lat, lon, zoom) {
+function googleMaps(lat, lon, zoom) {
   // http://maps.google.com/?q=${lat},${lon}&t=h&z=${zoom}
   return `https://www.google.com/maps/place/${lat},${lon}/@${lat},${lon},${zoom}z`;
 }
 
-app.get('/latest', async (req, res) => {
-  let locations = await all("SELECT lat, lon FROM locations");
-
-  if (locations.length > 0) {
-    const { lat, lon } = locations[locations.length - 1];
-    return res.redirect(google_maps(lat, lon, 13));
-  } else {
-    return res.status(404).send();
+app.get('/latest', async (req, res, next) => {
+  try {
+    const latest = await get("SELECT lat, lon, tst, DATETIME(tst, 'auto') AS dt FROM locations ORDER BY tst DESC LIMIT 1");
+    if (latest) {
+      return res.redirect(googleMaps(latest.lat, latest.lon, 15));
+      //return res.json(latest)
+    } else {
+      return res.status(400).send();
+    }
+  } catch (err) {
+    next(err)
   }
 });
 
-app.get('/locations', async (req, res) => {
-  let locations = await all("SELECT lat, lon FROM locations ORDER BY RANDOM()");
-  return res.json(locations);
+app.get('/locations', async (req, res, next) => {
+  try {
+    let locations = await all("SELECT lat, lon FROM locations ORDER BY RANDOM()");
+    return res.json(locations);
+  } catch (err) {
+    next(err)
+  }
 })
 
-app.post('*', async (req, res) => {
+/*
+app.post('*', async (req, res, next) => {
   const data = req.body;
 
   try {
     if (data && data._type === "location") {
       const { lat, lon, tst, tid } = data;
-      await write_location(lat, lon, tst, tid);
+      await writeLocation(lat, lon, tst, tid);
     }
+  
+    return res.json([]);
   } catch (e) {
     console.log(e);
   }
-  return res.json([]);
 })
+*/
+
+
+app.post('*', async (req, res, next) => {
+  try {
+    const data = req.body;
+
+    if (data && data._type === "location") {
+      const { lat, lon, tst, tid } = data;
+      await writeLocation(lat, lon, tst, tid);
+    }
+
+    return res.status(200).json([]);
+  } catch (err) {
+    console.log(err)
+    if (err.code == "SQLITE_CONSTRAINT") {
+      console.error(err.code)
+      return res.status(200).json([]);
+    } else {
+      console.log("post test")
+      return next(err);
+    }
+  }
+})
+
+const errorHandler = (err, req, res, next) => {
+  const errStatus = err.statusCode || 500;
+  console.error(err)
+  console.log("error handler");
+  return res.status(500).json([])
+}
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Location Tracker listening on port ${port}`)
